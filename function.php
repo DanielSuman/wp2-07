e5973ac1bae3e14b4b452ab11146ac5714c55bcf
daniel <daniel.suman@student.ossp.cz>
2021-12-17 10:37:09 +0100

    Added the funcs OwO

---
+++ function.php
@@ -0,0 +1,12 @@
+<?php 
+
+function myDummyFunction(int $age) {
+    if ($age >= 19) {
+        echo "Jsi dospělý";
+    } else {
+        echo "Jsi moc mladý, YOU SHALL NOT PASS!";
+    }
+    echo "<br>";
+}
+
+?>
